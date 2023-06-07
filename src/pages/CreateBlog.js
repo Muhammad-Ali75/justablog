7656abb3af5513b010b2b154e1d41bda95fdb0fb
Ali Sufyan <galisufyan327@gmail.com>
2023-06-07 19:11:54 +0500

    add form to create blog page

--- src/pages/CreateBlog.js
+++ src/pages/CreateBlog.js
@@ -1,7 +1,51 @@
+import { useState } from "react";
+
 function CreateBlog() {
+  const [blogData, setBlogData] = useState({
+    title: "",
+    body: "",
+    author: "mario",
+  });
+  function changeBlogData({ key, value }) {
+    setBlogData((prevState) => {
+      return { ...prevState, [key]: value };
+    });
+  }
   return (
     <div className="create">
       <h2>Add a new Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={blogData.title}
+          onChange={(e) =>
+            changeBlogData({ key: "title", value: e.target.value })
+          }
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={blogData.body}
+          onChange={(e) =>
+            changeBlogData({ key: "body", value: e.target.value })
+          }
+        >
+          {" "}
+        </textarea>
+        <label>Blog author:</label>
+        <select
+          value={blogData.author}
+          onChange={(e) =>
+            changeBlogData({ key: "author", value: e.target.value })
+          }
+        >
+          <option value="mario">Mario</option>
+          <option value="yoshi">Yoshi</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 }
