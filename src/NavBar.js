f1cdaa3c13e7f2a670f105835c05af97e54c4b8a
Ali Sufyan <galisufyan327@gmail.com>
2023-06-07 12:35:29 +0500

    Added NavBar

---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <h1>Just A Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">Create Blog</a>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
