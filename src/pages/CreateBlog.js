6da9dcfe28fae21a8edee4fe95d6991f440108c8
Muhammad Ali <ali.muhammad751935@gmail.com>
2023-06-08 11:59:52 +0500

    added a loader in create Blog

--- src/pages/CreateBlog.js
+++ src/pages/CreateBlog.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
-
+import { BallTriangle } from "react-loader-spinner";
 function CreateBlog() {
+  const [isPending, setIsPending] = useState(false);
   const [blogData, setBlogData] = useState({
     title: "",
     body: "",
@@ -13,8 +14,18 @@ function CreateBlog() {
   }
   function handleSubmit(e) {
     e.preventDefault();
+    setIsPending(true);
+    setTimeout(() => {
+      fetch("http://localhost:5000/blogs", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(blogData),
+      }).then(() => {
+        setIsPending(false);
 
-    console.log("FORM DATA:::", blogData);
+        console.log("POSTed");
+      });
+    }, 1000);
   }
   return (
     <div className="create">
@@ -49,7 +60,19 @@ function CreateBlog() {
           <option value="mario">Mario</option>
           <option value="yoshi">Yoshi</option>
         </select>
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && (
+          <div className="loader">
+            <BallTriangle
+              height={25}
+              width={25}
+              radius={5}
+              color="White"
+              ariaLabel="ball-triangle-loading"
+              visible={true}
+            />
+          </div>
+        )}
       </form>
     </div>
   );
